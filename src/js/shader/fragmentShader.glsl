f355ac09ac7077f295f60a53c133d9c3e68afe9e
keisuke sato <corydoras121@gmail.com>
2023-10-28 09:24:14 +0900

    [update]パーティクルを丸くする

--- src/js/shader/fragmentShader.glsl
+++ src/js/shader/fragmentShader.glsl
@@ -1,3 +1,8 @@
 void main() {
- gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+
+  if(distance(gl_PointCoord, vec2(0.5, 0.5)) > 0.5) {
+    discard;
+  }
+
+  gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
 }
--- src/js/shader/vertexShader.glsl
+++ src/js/shader/vertexShader.glsl
@@ -1,12 +1,9 @@
 
 void main() {
-  // vec4 mvPosition = modelViewMatrix * vec4( position, 1.0 );
-  // gl_Position = projectionMatrix * mvPosition;
-  // gl_PointSize = 1000.0 / -mvPosition.z;
-  // vec4 modelPosition = modelMatrix * vec4(position, 1.0);
-
   vec3 pos = position;
   vec4 modelPosition = modelViewMatrix * vec4(pos, 1.0);
+
   gl_Position = projectionMatrix * modelPosition;
-  gl_PointSize = 1000.0 / -modelPosition.z;
+  // gl_PointSize = 2.0;
+  gl_PointSize = 3000.0 / -modelPosition.z;
 }
